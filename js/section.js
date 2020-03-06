372ca44be5dfa7c54886f1fd56d5cd8832d78514
yuhsuan <snowsharon18@gmail.com>
2020-03-06 16:59:34 +0800

    click js

--- js/section.js
+++ js/section.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
+  $('.content:not(.active)').hide();
 
 
+  $('.title').click(function(e){
+    var index = $(this).closest('div').index();
 
+    $(this).addClass('active');
+    $(this).closest('div').siblings().find('a').removeClass('active');
+
+    $('.content').hide().eq(index).show();
+  });
 });
